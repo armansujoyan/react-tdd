94189c2901dece639dbf8ab9e1afe19e59a3cd22
Arman Sujoyan <asujoyan@gmail.com>
2018-12-30 02:24:51 +0400

    Add Gift tests for gift component

---
+++ giftgiver/src/components/Gift.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Gift from './Gift';
+
+describe('Gift component tests', () => {
+  const gift = shallow(<Gift />);
+
+  it('Renders properly', () => {
+    expect(gift).toMatchSnapshot();
+  });
+
+  it('Initializes a person and present in `state`', () => {
+    expect(gift.state()).toEqual({ person: '', present: ''});
+  })
+
+  describe('When typing in person input', () => {
+    const person = 'Sister';
+
+    beforeEach(() => {
+      gift.find('.input-person').simulate('chnage', { target: { value: person}});
+    })
+
+    it('Updates the person in state', () => {
+      expect(gift.state().person).toEqual(person);
+    })
+  })
+});
