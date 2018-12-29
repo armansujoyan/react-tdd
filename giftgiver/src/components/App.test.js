efde897f8c24fc1377159e4e853057f074a7dace
Arman Sujoyan <asujoyan@gmail.com>
2018-12-30 02:24:06 +0400

    Move test adapter config to test setup file

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import App from './App';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
 
 const app = shallow(<App />);
 
-it('Renders App component correctly', () => {
-  expect(app).toMatchSnapshot();
-});
+describe('App component tests', () => {
+  it('Renders App component correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
 
-it('Initialized the `state` with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([]);
-});
+  it('Initialized the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it('Adds new gift to `state` when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
-  expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
+  describe('Clicking the `add gift` button', () => {
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    })
+
+    afterEach(() => {
+      app.setState({ gifts: [] })
+    });
+
+    it('Adds new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{ id: 1 }]);
+    });
+
+    it('Adds a new gift to rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    })
+  })
+})
