3b2cf9cb7282887fefa112f31f4f03fc057deddc
Arman Sujoyan <asujoyan@gmail.com>
2018-12-30 18:29:05 +0400

    Create a deposit action.

--- lootcheck/src/actions/balance.js
+++ lootcheck/src/actions/balance.js
@@ -1,3 +1,4 @@
 import * as constants from './constants';
 
 export const setBalance = balance => ({ type: constants.SET_BALANCE, balance });
+export const deposit = deposit => ({ type: constants.DEPOSIT, deposit });
--- lootcheck/src/actions/balance.test.js
+++ lootcheck/src/actions/balance.test.js
@@ -4,7 +4,15 @@ import * as actions from './balance';
 it('creates an action to set the balance', () => {
   const balance = 0;
 
-  const expectedAction = { type: constants.SET_BALANCE , balance };
+  const expectedAction = { type: constants.SET_BALANCE, balance };
 
   expect(actions.setBalance(balance)).toEqual(expectedAction);
+});
+
+it('creates an action to deposit to a balance', () => {
+  const deposit = 10;
+
+  const expectedAction = { type: constants.DEPOSIT, deposit}
+
+  expect(actions.deposit(deposit)).toEqual(expectedAction);
 });
