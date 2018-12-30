39c821ff59998eefe8c2d4e512e91a5c877e3790
Arman Sujoyan <asujoyan@gmail.com>
2018-12-30 18:54:25 +0400

    Add local state to wallet and change

--- lootcheck/src/components/Wallet.js
+++ lootcheck/src/components/Wallet.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      balance: 0
+    }
+  }
+
+  updateBalance = event => {
+    this.setState({
+      balance: parseInt(event.target.value, 10)
+    })
+  }
+
   render() {
     return (
       <div>
         <h2 className='balance'>Wallet Balance: {this.props.balance}</h2>
+        <br/>
+        <input className='input-wallet' onChange={this.updateBalance}/>
       </div>
     )
   }
--- lootcheck/src/components/Wallet.test.js
+++ lootcheck/src/components/Wallet.test.js
@@ -12,5 +12,21 @@ describe('Wallet component', () => {
 
   it('displays balance from props', () => {
     expect(wallet.find('.balance').text()).toEqual('Wallet Balance: 20');
+  });
+
+  it('renders the input to deposit or withdraw', () => {
+    expect(wallet.find('.input-wallet').exists()).toBe(true);
   })
+
+  describe('when the user types into the wallet input', () => {
+    const userBalance = '21';
+
+    beforeEach(() => {
+      wallet.find('.input-wallet').simulate('change', { target: { value: userBalance }});
+    });
+
+    it('updates the local wallet balance in the `state` and converts to number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+    });
+  });
 });
